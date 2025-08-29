3916d995055c03d67f14ae20e158c4dac169df68
Kinas01 <bestmedi50@gmail.com>
2025-08-29 21:24:28 +0100

    Added Js to make box functional and updated html and css content

--- devUp.js
+++ devUp.js
@@ -134,3 +134,18 @@ clearBtn.addEventListener( "click", () => {
     countBtn.textContent = count;
 });
 
+const boxes = document.querySelectorAll('.dif-colors');
+const boxButton = document.querySelector('.box-button');
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+boxButton.addEventListener('click', () => {
+  boxes.forEach(box => {
+    box.style.backgroundColor = getRandomColor();
+  });
+});
